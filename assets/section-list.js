672f005e42634c924bb82bd1db844cb85c2b9bde
extmaster <111635361+extmaster@users.noreply.github.com>
2026-02-05 12:18:40 +0300

    Add files via upload

---
+++ assets/section-list.js
@@ -0,0 +1,34 @@
+const sections = [
+    {
+        "file": "01-concepts.html",
+        "name": "Concepts"
+    },
+    {
+        "file": "02-zed.html",
+        "name": "Zed"
+    },
+    {
+        "file": "03-zed-history.html",
+        "name": "Zed History"
+    },
+    {
+        "file": "04-opencode.html",
+        "name": "Opencode"
+    },
+    {
+        "file": "05-github.html",
+        "name": "Github"
+    },
+    {
+        "file": "06-tips.html",
+        "name": "Tips"
+    },
+    {
+        "file": "07-mcp-list.html",
+        "name": "Mcp List"
+    },
+    {
+        "file": "08-useful-links.html",
+        "name": "Useful Links"
+    }
+];
